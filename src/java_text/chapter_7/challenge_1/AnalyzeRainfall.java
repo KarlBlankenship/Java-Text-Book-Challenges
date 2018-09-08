8dc53f7cce4d32f4f609b692187e4578a3628ac2
Karl Blankenship <Karleb777@gmail.com>
2018-09-08 12:19:44 -0500

    Added main class for rainfall analysis chapter 7 challenge 1

---
+++ src/java_text/chapter_7/challenge_1/AnalyzeRainfall.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_text.chapter_7.challenge_1;
+
+/**
+ *
+ * @author Karl
+ */
+public class AnalyzeRainfall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
