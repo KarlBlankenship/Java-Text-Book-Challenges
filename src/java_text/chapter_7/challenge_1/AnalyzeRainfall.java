c65582741d87d5e31b8ec6a4bc656914ff862ef2
Karl Blankenship <Karleb777@gmail.com>
2018-09-08 12:35:37 -0500

    Completed the Analyze file for Chap 7 Chal 1

--- src/java_text/chapter_7/challenge_1/AnalyzeRainfall.java
+++ src/java_text/chapter_7/challenge_1/AnalyzeRainfall.java
@@ -6,7 +6,8 @@
 package java_text.chapter_7.challenge_1;
 
 /**
- *
+ * This is the file contains the main method which will analyze rainfall
+ * data using the Rainfall class.
  * @author Karl
  */
 public class AnalyzeRainfall {
@@ -15,7 +16,31 @@ public class AnalyzeRainfall {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Create the rainfall object.
+        Rainfall rf = new Rainfall();
+        
+        // Add made-up rainfall amounts to each month.
+        for (int m=0; m<12; m++) {
+            rf.setMonthRainfall(m, 10+m);
+            System.out.println("Month: " + m + "   Rainfall: " + (m+10));
+        }
+        
+        // Get and display total rainfall for the year.
+        System.out.println("Yearly Total Rainfall: " +
+                rf.getYearlyRainfallTotal());
+        
+        // Get and display average rainfall for the year.
+        System.out.println("Average Monthly Rainfall: " +
+                rf.getAverageMonthlyRainfall());
+        
+        // Get and display the wettest month.
+        System.out.println("Wettest Month: " +
+                rf.getWettestMonth());
+        
+        //Get and display the driest month.
+        System.out.println("Driest Month: " +
+                rf.getDriestMonth());
     }
     
 }
