91a9d95cd7c13e61a7f4523095ac8d8f47de9e09
Karl Blankenship <Karleb777@gmail.com>
2018-09-09 09:52:58 -0500

    Completed Area Class in chap 8 chal 1

--- src/java_text/chapter_8/challenge_1/Area.java
+++ src/java_text/chapter_8/challenge_1/Area.java
@@ -5,10 +5,28 @@
  */
 package java_text.chapter_8.challenge_1;
 
+import java.lang.Math.*;
+
 /**
- *
+ * Area class with overload static methods for finding the area of a circle,
+ * rectangle and cylinder. Static methods can be called without creating an
+ * instance of the class. Simply use the ClassName.Method to execute the method.
  * @author Karl
  */
 public class Area {
+
+    // Create static method for finding the area of a circle.
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
     
+    // Create static overload method for finding the area of a rectangle.
+    public static int calculateArea(int width, int length) {
+        return width * length;
+    }
+    
+    // Create static overload method for finding the area of a cylinder.
+    public static double calculateArea(double radius, double height) {
+        return Math.PI * radius * radius * height;
+    }
 }
