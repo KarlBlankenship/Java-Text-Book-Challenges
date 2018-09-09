a3e71f41dc3b05146dc99c051d095d03db7e5401
Karl Blankenship <Karleb777@gmail.com>
2018-09-09 11:30:51 -0500

    Completed ReverseString chap 9 chal 1.

--- src/java_text/chapter_9/challenge_1/ReverseString.java
+++ src/java_text/chapter_9/challenge_1/ReverseString.java
@@ -5,6 +5,8 @@
  */
 package java_text.chapter_9.challenge_1;
 
+import java.util.Scanner;
+
 /**
  * This class will prompt the user to enter a string which will then be 
  * displayed with the characters in reverse order.
@@ -17,6 +19,29 @@ public class ReverseString {
      */
     public static void main(String[] args) {
         
+        String inputString; // String reference variable to hold input string.
+        Scanner keyboard = new Scanner(System.in); // New Scanner object.
+        
+        // Prompt user to enter a string.
+        System.out.println("Please enter a string.");
+        
+        // Read string upon enter.
+        inputString = keyboard.nextLine();
+        
+        System.out.println(reverseOrder(inputString));
+        
     }
+    
+    public static String reverseOrder(String input) {
+            String outputString = ""; // Create the return string.
+            
+            String[] tokens = input.split(""); // Split string into an array.
+            
+            // Convert tokens back into a string in reverse order.
+            for (int i=tokens.length - 1; i >= 0;i--)
+                outputString = outputString + (tokens[i]);
+            
+            return outputString; // Return the reversed output string.
+        }
     
 }
