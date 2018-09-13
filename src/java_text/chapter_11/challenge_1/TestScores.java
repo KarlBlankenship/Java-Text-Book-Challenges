ad99ee419a6ffeaf13123945fc70121e94c435e4
Karl Blankenship <Karleb777@gmail.com>
2018-09-13 17:04:37 -0500

    Completed TestScore Class with throws exception chap 11 chal 1.

--- src/java_text/chapter_11/challenge_1/TestScores.java
+++ src/java_text/chapter_11/challenge_1/TestScores.java
@@ -29,7 +29,25 @@ public class TestScores {
         this.testScores = testScores;
     }
     
-    
+    public double averageTestScore(int[] ts) {
+        double average = 0; // Initialize the variable for the average.
+        int sum = 0; // Sum of array.
+        
+        // Sum all the test scores in the array.
+        for (int i = 0; i < ts.length; i++) {
+            if (ts[i] < 0 || ts[i] > 100) {
+                throw new IllegalArgumentException("One of your test scores" + 
+                        " is negative or greater than 100!");
+            }
+            sum = sum + ts[i];
+        }
+        
+        // Calculate the average.
+        average = sum / ts.length;
+        
+        // Return the average.
+        return average; 
+    }
     
     
 }
