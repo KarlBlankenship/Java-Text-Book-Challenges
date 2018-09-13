18e868a206cac45a6fb8b268a376b708220ff0e2
Karl Blankenship <Karleb777@gmail.com>
2018-09-13 17:18:53 -0500

    Corrected Class and tested via main method chap 11 chal 1.

--- src/java_text/chapter_11/challenge_1/AnalyzeTestScores.java
+++ src/java_text/chapter_11/challenge_1/AnalyzeTestScores.java
@@ -6,7 +6,8 @@
 package java_text.chapter_11.challenge_1;
 
 /**
- *
+ * This main method class will utilize the TestScore Class to analyze test
+ * scores.
  * @author Karl
  */
 public class AnalyzeTestScores {
@@ -15,7 +16,20 @@ public class AnalyzeTestScores {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    // Create an array of valid test scores.
+    int[] validTestScores = {20, 40, 60, 80};
+    
+    // Create an array of invalid scores.
+    int[] invalidTestScores = {20, 40, 160, 80};
+
+    // Create a TestScore object
+    TestScores vts = new TestScores(validTestScores);
+    System.out.println(vts.averageTestScore());
+    
+    // Create an invlid TestScore instance.
+    TestScores ivts = new TestScores(invalidTestScores);
+    System.out.println(ivts.averageTestScore());
     }
     
 }
--- src/java_text/chapter_11/challenge_1/TestScores.java
+++ src/java_text/chapter_11/challenge_1/TestScores.java
@@ -32,24 +32,24 @@ public class TestScores {
     /**
      * This method will return an average test score and will throw an 
      * exception if one of the test scores is invalid.
-     * @param ts
+     * @param testScores
      * @return average.
      */
-    public double averageTestScore(int[] ts) {
-        double average = 0; // Initialize the variable for the average.
+    public double averageTestScore() {
+        double average; // Initialize the variable for the average.
         int sum = 0; // Sum of array.
         
         // Sum all the test scores in the array.
-        for (int i = 0; i < ts.length; i++) {
-            if (ts[i] < 0 || ts[i] > 100) {
+        for (int i = 0; i < testScores.length; i++) {
+            if (testScores[i] < 0 || testScores[i] > 100) {
                 throw new IllegalArgumentException("One of your test scores" + 
                         " is negative or greater than 100!");
             }
-            sum = sum + ts[i];
+            sum = sum + testScores[i];
         }
         
         // Calculate the average.
-        average = sum / ts.length;
+        average = sum / testScores.length;
         
         // Return the average.
         return average; 
