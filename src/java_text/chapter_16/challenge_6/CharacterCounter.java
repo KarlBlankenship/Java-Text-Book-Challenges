b6c3eaa8f7df8be83bb6cd6fec6dea140d24aacf
Karl Blankenship <Karleb777@gmail.com>
2018-09-28 14:33:13 -0500

    Finished cleaning up and commenting. chap16 chal6

--- src/java_text/chapter_16/challenge_6/CharacterCounter.java
+++ src/java_text/chapter_16/challenge_6/CharacterCounter.java
@@ -17,19 +17,29 @@ public class CharacterCounter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char[] charArray = {'a','b','c','i','d','e','f','g','t','y'};
+        // Initialize the character array to be searched.
+        char[] charArray = {'t','b','a','i','d','t','f','g','t','t'};
+        // Set a search character.
         char target = 'a';
-        System.out.println(counter(charArray, target, charArray.length-1));
+        //Print out search results.
+        System.out.println("The char " + target + " was found " +
+                counter(charArray, target, 0) + " times in the array.");
     }
     
+    /**
+     * counter method will accept an array of characters and count
+     * how many times a specified character is present.
+     * @param input array of characters
+     * @param x specified search character
+     * @param index starting index for the search
+     * @return count of how many times the specified character was present
+     */
     private static int counter(char[] input, char x, int index) {
-        if (index == 0 && input[index] == x)
-            return 1;
-        else if (index == 0 && input[index] != x)
+        if (index == input.length)
             return 0;
         else if (input[index] == x)
-            return 1 + counter(input, x, index - 1);
+            return 1 + counter(input, x, index + 1);
         else
-            return counter(input, x, index - 1);        
+            return counter(input, x, index + 1);  
     }
 }
