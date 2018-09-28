d3d8fcd49053ec076d52e9c2b6a58b3f12944264
Karl Blankenship <Karleb777@gmail.com>
2018-09-28 14:05:30 -0500

    Started CharacterCounter class with counter method. chap16 chal6

--- src/java_text/chapter_16/challenge_6/CharacterCounter.java
+++ src/java_text/chapter_16/challenge_6/CharacterCounter.java
@@ -1,21 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Write a method that uses recursion to count the number of times a
+ * specific character occurs in an array of characters. Demonstrate the
+ * method in a program.
  */
 package java_text.chapter_16.challenge_6;
 
 /**
- *
+ * Public class for launching the main method and the character 
+ * counter method.
  * @author Karl
  */
 public class CharacterCounter {
 
     /**
+     * Main method.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
+    private int counter(char[] input, char x) {
+        
+    }
 }
