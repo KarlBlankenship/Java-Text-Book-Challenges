0818baba84672f41f3e18acf3e879c3f342d5e07
Karl Blankenship <Karleb777@gmail.com>
2018-09-29 16:19:28 -0500

    Completed the fibonacci method with just recursion.

--- src/java_text/chapter_16/challenge_0/DynamicFibonacci.java
+++ src/java_text/chapter_16/challenge_0/DynamicFibonacci.java
@@ -6,16 +6,30 @@
 package java_text.chapter_16.challenge_0;
 
 /**
- *
+ * This program will calculate nth fibonacci number by utilizing memoizatiom
+ * and dynamic programming.
  * @author Karl
  */
 public class DynamicFibonacci {
 
     /**
+     * Main method calls fibonacci method.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int fib = 5;
+        System.out.println("The " + fib + "th Fibonacci number is " +
+                fibonacci(fib) + ".");
+        
+    }
+    
+    private static int fibonacci(int n) {
+        
+        if(n == 0 || n == 1)
+            return n;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
     }
     
 }
