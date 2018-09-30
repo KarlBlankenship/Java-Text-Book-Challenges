27d04f4cbd9b3522069d94d82c9c13ad920becb1
Karl Blankenship <Karleb777@gmail.com>
2018-09-30 11:54:05 -0500

    Started a javafx recreation of an example window. chap 15 chal 1

---
+++ src/java_text/chapter_15/challenge_1/NestedLayouts1.java
@@ -0,0 +1,21 @@
+/*
+ * Chapter 15 Challenge 1
+ * Nested Layouts 1
+ * Write a program that creates the layout of Figure 15-12.
+ */
+package java_text.chapter_15.challenge_1;
+
+/**
+ *
+ * @author Karl
+ */
+public class NestedLayouts1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
