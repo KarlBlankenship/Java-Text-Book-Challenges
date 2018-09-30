5b93ad072ef7e9a381160c3dd7502b8a877a4d5b
Karl Blankenship <Karleb777@gmail.com>
2018-09-30 12:24:07 -0500

    Completed the example. Centered labels and buttons and increased the width of the window. chap 15 chal1

--- src/java_text/chapter_15/challenge_1/NestedLayouts1.java
+++ src/java_text/chapter_15/challenge_1/NestedLayouts1.java
@@ -6,13 +6,13 @@
 package java_text.chapter_15.challenge_1;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 /**
  * This application will recreate a sample grid with a centered label
@@ -33,7 +33,7 @@ public class NestedLayouts1 extends Application {
     public void start(Stage stage) {
         
         // Create the label.
-        Label label = new Label("Nested Layout");
+        Label label = new Label("This is a label");
         
         // Create the buttons.
         Button button1 = new Button("Button 1");
@@ -41,23 +41,24 @@ public class NestedLayouts1 extends Application {
         
         // Create the horizontal box.
         HBox buttonBox = new HBox(10);
+        buttonBox.setAlignment(Pos.CENTER);
         
         // Add buttons to the HBox.
         buttonBox.getChildren().addAll(button1, button2);
         
         // Create the vertical box.
         VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
         
         // Add buttonBox and label to vertical box.
         vBox.getChildren().addAll(label, buttonBox);
         
         // Set the scene by adding the vertical box.
-        Scene scene = new Scene(vBox, 240, 80);
+        Scene scene = new Scene(vBox, 260, 80);
         
         // Add scene to stage and show stage.
         stage.setTitle("Nested Layout");
         stage.setScene(scene);
         stage.show();
     }
-    
 }
