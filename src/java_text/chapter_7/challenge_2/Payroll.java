dc007699fa16416b12da7eebcee485421189fa81
Karl Blankenship <Karleb777@gmail.com>
2018-10-02 16:05:45 -0500

    Completed code in PayRoll class. chap7 chal2

--- src/java_text/chapter_7/challenge_2/Payroll.java
+++ src/java_text/chapter_7/challenge_2/Payroll.java
@@ -12,7 +12,52 @@ package java_text.chapter_7.challenge_2;
  */
 public class Payroll 
 {
+    // Employee ID array is initialized with 7 provided employee ID's.
     private int[] employeeID = {5658845, 4520125, 7895122, 8777541,
                                 8451277, 1302850, 7580489};
-    private int 
+    private int[] hours = new int[7];           // Hold employee hours.
+    private double[] payRate = new double[7];   // Hold employee pay rate.
+    private double[] wages = new double[7];     // Hold employee wages.
+
+    public int[] getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int[] employeeID) {
+        this.employeeID = employeeID;
+    }
+    
+    public int[] getHours() {
+        return hours;
+    }
+
+    public void setHours(int[] hours) {
+        this.hours = hours;
+    }
+    
+    public double[] getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double[] payRate) {
+        this.payRate = payRate;
+    }
+    
+    public double calculateGrossWages(int eID)
+    {
+        double grossWages = -1;
+        for (int index : employeeID)
+        {
+            if (employeeID[index] == eID) 
+            {
+                wages[index] = hours[index] * payRate[index];
+                grossWages = wages[index];
+            }
+        }
+        return grossWages;
+    }
+
+    
+    
+    
 }
