38fcfefcd581cbc247793e68cf3357a6d570587b
Karl Blankenship <Karleb777@gmail.com>
2018-10-02 16:36:38 -0500

    Started the main method to querry for payrate and hours for each employee id. chap 7 chal 2

--- src/java_text/chapter_7/challenge_2/demonstratePayroll.java
+++ src/java_text/chapter_7/challenge_2/demonstratePayroll.java
@@ -5,6 +5,8 @@
  */
 package java_text.chapter_7.challenge_2;
 
+import java.util.*;
+
 /**
  * This program will demonstrate the payroll class.
  * @author Karl
@@ -21,9 +23,38 @@ public class demonstratePayroll
         // Create a payroll object.
         Payroll payroll = new Payroll();
         
+        // Create the keyboard and System in.
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Create an int variable for keyboard input.
+        int keyInt;
+        
+        // Create a double variable for keyboard input.
+        double keyDouble;
+
+        // Create boolean valid flag for data entry.
+        boolean isValid = false;
+        
+        
         for (int index : payroll.getEmployeeID())
         {
+            System.out.println("Please enter hours for employee ID " +
+                                payroll.getEmployeeID(index) + ": ");
+            while (!isValid)
+            {
+                keyInt = keyboard.nextInt();
+                if (keyInt >= 0)
+                {
+                    payroll.setHours(index, keyInt);
+                    isValid = true;
+                }
+                else
+                    System.out.println("Hours must be a non-negative integer.");
+            }    
+                
+            // Reset the valid flag and ask for pay rate to be entered
             
+            // calculate gross pay, update object and display the gross pay.
         }
     }
     
