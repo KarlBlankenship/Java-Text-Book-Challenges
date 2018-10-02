005cca16e437020728d2f0a7a2c4f062a142fc73
Karl Blankenship <Karleb777@gmail.com>
2018-10-02 15:42:20 -0500

    Started a payroll class. chap 7 chal 2

---
+++ src/java_text/chapter_7/challenge_2/Payroll.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_text.chapter_7.challenge_2;
+
+/**
+ * The Payroll class will have fields for an employee ID, hours worked,
+ * pay rate and wages. There will be a method for calculating gross pay.
+ * @author Karl
+ */
+public class Payroll 
+{
+    private int[] employeeID = {5658845, 4520125, 7895122, 8777541,
+                                8451277, 1302850, 7580489};
+    private int 
+}
