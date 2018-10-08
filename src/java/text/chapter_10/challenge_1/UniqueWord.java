ce21df72ace65e86968d6a42ef495d92e94be78f
Karl Blankenship <Karleb777@gmail.com>
2018-10-07 19:12:04 -0500

    Started a program that will read a text line and display each unique word. chap 19 chal 1

---
+++ src/java/text/chapter_10/challenge_1/UniqueWord.java
@@ -0,0 +1,22 @@
+/*
+ * Write an application that reads a line of input from the keyboard,
+ * and then displays each unique word that was entered, sorted in 
+ * ascending order. You can do this by tokenizing the line of input
+ * and adding each token to an appropriate Set object.
+ */
+package java.text.chapter_10.challenge_1;
+
+/**
+ *
+ * @author Karl
+ */
+public class UniqueWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
