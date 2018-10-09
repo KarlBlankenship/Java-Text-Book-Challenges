6261d0360fd18da8f955862e27fed283a12d8475
Karl Blankenship <Karleb777@gmail.com>
2018-10-09 14:08:11 -0500

    Started a problem to create a generic double linked list class with specific methods and test it. Chap 20 Chal 1

---
+++ src/java_text/chapter_20/challenge_1/GenericLinkedList.java
@@ -0,0 +1,20 @@
+/*
+ * Modify the doubly linked list class presented in this chapter so it 
+ * works with generic types. Add the following methods drawn from
+ * the java.util.List interface:
+ *  - void clear(): remove all elements from the list.
+ *  - E get(int index): return the element at position index in the list.
+ *  - E set(int index, E element): replace the element at the specified
+ *    position with the specified element anf return the previous element.
+ * Test your generic linked list class by processing a list of numbers
+ * of type double.
+ */
+package java_text.chapter_20.challenge_1;
+
+/**
+ *
+ * @author Karl
+ */
+public class GenericLinkedList {
+    
+}
