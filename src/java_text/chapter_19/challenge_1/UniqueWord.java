00d3dd30ec333bdb220de64bf83bf12903e5b7d4
Karl Blankenship <Karleb777@gmail.com>
2018-10-08 20:03:11 -0500

    Completed the unique word sorted. Used a TreeSet to avoid duplicates and to automatically sort into ascending order. Chap 19 Chal 1

--- src/java_text/chapter_19/challenge_1/UniqueWord.java
+++ src/java_text/chapter_19/challenge_1/UniqueWord.java
@@ -22,20 +22,32 @@ public class UniqueWord
      */
     public static void main(String[] args) 
     {
-        String line; // Reference variable for line entered by keyboard.
-        
+        // Have user enter a line of text, dibide into words and save into
+        // a text array.
         // Create a scanner reference variable for storing the keyboard input.
         Scanner keyboard = new Scanner(System.in);
-        
         // Ask the user to enter a line of words.
         System.out.println("Please enter a line of words...");
-        
         // Retrieve the keyboard input.
-        line = keyboard.nextLine();
+        String line = keyboard.nextLine();
+        System.out.println("This is the line you entered:");
+        System.out.println(line);
+        // Split the line into individual words by space.
+        String[] words = line.split(" "); 
         
+        // Create a TreeSet and store the words in it.
+        // A Set will not allow duplicate words and a TreeSet
+        // will store the words in ascending order.
+        SortedSet<String> wordSet = new TreeSet<>();
+        for (String word : words)
+        {
+            wordSet.add(word);
+        }
         
-        
-        
+        // Display the words in ascending order.
+        System.out.println("\nThese are the words you entered");
+        System.out.println("without duplicate and in ascending");
+        System.out.println("order:");
+        System.out.println(wordSet);
     }
-    
 }
