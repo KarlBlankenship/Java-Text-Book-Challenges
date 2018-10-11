527ecfb4a00a5e893daa4263bdd78ef3f7128e08
Karl Blankenship <Karleb777@gmail.com>
2018-10-11 13:23:53 -0500

    Started a linked list exercise to write a list reverse method and a list sort method without using recursion. Chap 20 Chal 2

---
+++ src/java_text/chapter_20/challenge_2/LinkedListSortAndReverse.java
@@ -0,0 +1,23 @@
+/*
+ * Modify the LinkedList1 class presented in this chapter by adding
+ * sort() and reverse() methods. The reverse method should reverse
+ * the order of the elements in the list, and the sort method
+ * rearranges the elements in alphabetical order. Do not use recursion
+ * to implement either of these operations.
+ */
+package java_text.chapter_20.challenge_2;
+
+/**
+ *
+ * @author Karl
+ */
+public class LinkedListSortAndReverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
