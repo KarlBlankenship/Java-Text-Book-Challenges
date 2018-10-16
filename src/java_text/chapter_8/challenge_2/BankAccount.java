506f58e4d39d89d6f7e750233757dd7c136c0e42
Karl Blankenship <Karleb777@gmail.com>
2018-10-16 14:47:09 -0500

    Modifying a BankAccount class to have a constructor that accepts BankAccount objects as the argument. Chap 8 Chal 2

---
+++ src/java_text/chapter_8/challenge_2/BankAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Add a copy constructor to the BankAccount class. This constructor
+ * should accept a BankAccount object as an argument. It should assign
+ * to the balance field the value in the arguments balance field. As a
+ * result, the new object will be a copy of the argument object.
+ */
+package java_text.chapter_8.challenge_2;
+
+/**
+ * The BankAccount class simulates a bank account.
+ * @author Karl
+ */
+public class BankAccount {
+    private double balance;     // Account balance.
+    
+}
