9333def004785fc77d3f4fa26f84a9e1a82f9531
Karl Blankenship <Karleb777@gmail.com>
2018-10-17 10:11:16 -0500

    Started a Word Counter challenge. Chap 9 Chal 2

---
+++ src/java_text/chapter_9/challenge_2/WordCounter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_text.chapter_9.challenge_2;
+
+/**
+ *
+ * @author Karl
+ */
+public class WordCounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
